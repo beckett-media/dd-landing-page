f6f8116e51f4bc6bcaf5ed163d84056b3c5ccca0
Rio's <macbookpro@Macbooks-MacBook-Pro.local>
2021-07-26 14:09:03 +0530

    submit email ui changed

--- src/components/UpperFooter/index.js
+++ src/components/UpperFooter/index.js
@@ -1,21 +1,33 @@
 import * as React from "react"
 
-
 import "./styles.css"
- 
 
 const UpperFooter = () => (
   <div className="py-5 upper-footer">
     <div className="container-md">
       <div className="row g-0 d-flex align-items-center justify-content-between">
-        <div className="col-12 col-md-6 col-lg-8 text-white py-1">
+        <div className="col-12 text-white py-3 text-center">
           Want to get the hottest drops, new releases, and free swag? Signup for
           our mailing list!
         </div>
-        <div className="col-12 col-md-6 col-lg-4 py-1">
-          <button type="button" className="upper-footer-button px-5 mx-0 mx-md-2">
-            Enter Email Address
-          </button>
+        <div className="col-12 py-1">
+          <form>
+            <div class="form-group d-block d-md-flex text-center text-md-left">
+              <input
+                type="email"
+                class="form-control"
+                id="UpperFooteremailInput"
+                aria-describedby="emailHelp"
+                placeholder="Enter Email Address"
+              />
+              <button
+                type="button"
+                className="upper-footer-button px-5 mx-0 mx-md-2"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
