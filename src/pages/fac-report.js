09fac0f73bc27d3c342258138c9b04bb8b7ef6ee
rashinp <rashin@7thpillar.com>
2021-07-30 01:41:25 +0530

    Removed Layout

--- src/pages/fac-report.js
+++ src/pages/fac-report.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { Router } from "@reach/router"
 import FacReport from "../components/FacReport"
-import Layout from "../components/Layout"
 import Seo from "../components/seo"
 const App = () => {
   return (
-    <Layout>
+    <>
       <Seo
         title="Due Dilly | Sports Card FAC"
         description="Factual Assessment of Card for Quality and Market Trends"
@@ -13,7 +12,7 @@ const App = () => {
       <Router>
         <FacReport path="/fac-report/:id" />
       </Router>
-    </Layout>
+    </>
   )
 }
 export default App
