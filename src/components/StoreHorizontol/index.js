7f5ae1cb1e9cad96ab43cd79f88ea8777002972d
Muhammad Waqas Ilyas <46993679+diode02@users.noreply.github.com>
2021-12-17 19:06:16 +0500

    New Style to latest store (#17)

--- src/components/StoreHorizontol/index.js
+++ src/components/StoreHorizontol/index.js
@@ -1,59 +1,94 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { CONFIG } from "../../constants/Config"
-const StoreHorizontal = ({ store, grade, packaging, authKey }) => {
+import { Avatar } from "antd"
+import "./style.css"
+import { capitalizeFirstEach, capitalizeFirstLetter } from "../../utils/string"
+
+const StoreHorizontal = ({ store, authKey }) => {
   return (
-    <Link
-      target="_blank"
-      href={
-        CONFIG.marketplace_url +
-        "/store/" +
-        store._id +
-        `${authKey ? `?auth=${encodeURIComponent(authKey)}` : ""}`
-      }
-    >
-      <div className="ps-product--horizontal">
-        <div className="ps-product__thumbnail">
-          {(store?.images.length && store?.images[0] && (
-            <img
-              width="100%"
-              alt={store.title}
-              src={CONFIG.base_url + "/" + store?.images[0]}
-            />
-          )) || <></>}
-        </div>
-        <div className="m-3 ps-product__player">
-          <>
-            <span>{store.title.toUpperCase()}</span>
-          </>
-          <hr />
-          <div
-            className="ps-product__meta-wrapper"
+    <article className="ps-block--store-2">
+      <div className="ps-block__content ">
+        <figure>
+          <h4>
+            <Link
+              href={
+                CONFIG.base_url +
+                "/store/" +
+                store._id +
+                `${authKey ? `?auth=${encodeURIComponent(authKey)}` : ""}`
+              }
+              target="_blank"
+            >
+              <a>{capitalizeFirstEach(store.title)}</a>
+            </Link>
+          </h4>
+
+          <div className="ps-block__rating mb-4"></div>
+          <p>{capitalizeFirstLetter(store.desc)}</p>
+          {store.phone && (
+            <p>
+              <i className="icon-telephone"></i> {store.phone}
+            </p>
+          )}
+        </figure>
+      </div>
+
+      <div className="ps-block__author mt-4">
+        <a
+          className="ps-block__user"
+          target="_blank"
+          href={
+            CONFIG.marketplace_url +
+            "/store/" +
+            store._id +
+            `${authKey ? `?auth=${encodeURIComponent(authKey)}` : ""}`
+          }
+          // style={{
+          //   backgroundImage: `url('${`${
+          //     store.images.length > 0
+          //       ? `${CONFIG.base_url}/${store.images[0]}`
+          //       : "/static/img/vendor/store/vendor-150x150.jpg"
+          //   }`}')`,
+          //   backgroundRepeat: "no-repeat",
+          //   // backgroundPosition: "20% 50%",
+          // }}
+        >
+          {/* <img
+                        src="/static/img/vendor/store/vendor-150x150.jpg"
+                        alt="Due Dilly"
+                    /> */}
+
+          <Avatar
+            size="large"
+            src={
+              store.images.length > 0
+                ? `${CONFIG.base_url}/${store.images[0]}`
+                : "/static/img/vendor-150x150.jpg"
+            }
+            alt="store image"
+          ></Avatar>
+        </a>
+        <Link
+          href={
+            CONFIG.marketplace_url +
+            "/store/" +
+            store._id +
+            `${authKey ? `?auth=${encodeURIComponent(authKey)}` : ""}`
+          }
+          target="_blank"
+        >
+          <a
+            className="ps-btn"
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "flex-end",
+              textDecoration: "none",
             }}
           >
-            <div className="ps-product__meta">
-              <div>
-                {/* <span>Packaging</span> */}
-                <p>
-                  {/* <strong className="text-secondary">{packaging.name}</strong> */}
-                </p>
-              </div>
-              <div>
-                {/* <span>Grade</span> */}
-                <p>
-                  {/* <strong className="text-secondary">{grade.name}</strong> */}
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* <div className="ps-product__price">${product.price.toFixed()}</div> */}
-        </div>
+            Visit Store
+          </a>
+        </Link>
       </div>
-    </Link>
+    </article>
   )
 }
 
