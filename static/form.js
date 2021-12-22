3d06cb915b7df0ead311fdc44a59fabad12bea92
Muhammad Waqas ilyas <qaskhan02@gmail.com>
2021-12-23 01:10:19 +0500

    new script added for husdon

--- static/form.js
+++ static/form.js
@@ -1,3 +1,15 @@
+// new script by Hudson
+(function (w, d, s, l, i) {
+  w[l] = w[l] || []
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : ""
+  j.async = true
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+  f.parentNode.insertBefore(j, f)
+})(window, document, "script", "dataLayer", "GTM-N5S4G39")
+
 var EhAPI = EhAPI || {}
 EhAPI.after_load = function () {
   EhAPI.set_account("s2q2h14jacgkkhmvvuip3s9hk2", "duedilly")
